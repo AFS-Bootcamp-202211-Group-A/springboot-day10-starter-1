3f21b327fb2bde914d777722b3d69017eeece1c5
Wongje4 <>
2022-12-09 11:48:10 +0800

    feat: employee response

---
+++ src/main/java/com/rest/springbootemployee/controller/dto/EmployeeResponse.java
@@ -0,0 +1,57 @@
+package com.rest.springbootemployee.controller.dto;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
+public class EmployeeResponse {
+
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public EmployeeResponse(String name, Integer age, String gender, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public EmployeeResponse() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+}
