5704a5f490102ed2e75b403b5de12b7fd225676a
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-09 18:09:01 +0800

    feat: use company mapper in company getAll

--- src/main/java/com/rest/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/rest/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.rest.springbootemployee.controller;
 
 import com.rest.springbootemployee.controller.dto.CompanyRequest;
+import com.rest.springbootemployee.controller.dto.CompanyResponse;
 import com.rest.springbootemployee.controller.mapper.CompanyMapper;
 import com.rest.springbootemployee.entity.Company;
 import com.rest.springbootemployee.service.CompanyService;
@@ -22,8 +23,9 @@ public class CompanyController {
     }
 
     @GetMapping
-    public List<Company> getAll() {
-        return companyService.findAll();
+    public List<CompanyResponse> getAll() {
+        List<Company> companies = companyService.findAll();
+        return companyMapper.toResponse(companies);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/rest/springbootemployee/controller/mapper/CompanyMapper.java
+++ src/main/java/com/rest/springbootemployee/controller/mapper/CompanyMapper.java
@@ -1,9 +1,13 @@
 package com.rest.springbootemployee.controller.mapper;
 
 import com.rest.springbootemployee.controller.dto.CompanyRequest;
+import com.rest.springbootemployee.controller.dto.CompanyResponse;
 import com.rest.springbootemployee.entity.Company;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CompanyMapper {
@@ -12,6 +16,19 @@ public class CompanyMapper {
         Company company = new Company();
         BeanUtils.copyProperties(companyRequest, company);
         return company;
+    }
+
+    public CompanyResponse toResponse(Company company){
+        CompanyResponse companyResponse = new CompanyResponse();
+        BeanUtils.copyProperties(company, companyResponse);
+        companyResponse.setEmployeesCount(Integer.valueOf(company.getEmployees().size()));
+        return companyResponse;
+    }
+
+    public List<CompanyResponse> toResponse(List<Company> companies){
+        return companies.stream()
+                .map(company -> toResponse(company))
+                .collect(Collectors.toList());
     }
 
 }
