563af10381860d5dbd8223e0027febaad9e9b71a
Alan Wan <alan.wan@oocl.com>
2022-12-09 11:39:04 +0800

    feat: Added employee response class to handle create_employee

--- src/main/java/com/rest/springbootemployee/controller/mapper/EmployeeMapper.java
+++ src/main/java/com/rest/springbootemployee/controller/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.rest.springbootemployee.controller.mapper;
 
 import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.rest.springbootemployee.controller.dto.EmployeeRequest;
+import com.rest.springbootemployee.controller.dto.EmployeeResponse;
 import com.rest.springbootemployee.entity.Employee;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
@@ -12,5 +13,12 @@ public class EmployeeMapper {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeRequest,employee);
         return employee;
+    }
+
+    public EmployeeResponse toResponse(Employee employee){
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        BeanUtils.copyProperties(employee, employeeResponse);
+
+        return employeeResponse;
     }
 }
